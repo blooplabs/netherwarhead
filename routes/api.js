589b3be6c386fae791342f1c4f70926b293730e5
Andrew Liu <aliuy8@gmail.com>
2015-03-01 17:22:39 -0800

    Start server-side reddit analyzer

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var reddit = require('../lib/api/reddit');
+var redditAnalyzer = require('../lib/analytics/reddit-analyzer');
 var router = express.Router();
 
 router.get('/pull', function (req, res, next) {
-  reddit.all(function (body) {
-    res.json(body);
+  reddit.top("all", function (body) {
+    redditAnalyzer.process(body, function (body) {
+      res.json(body);
+    });
   });
 });
 
