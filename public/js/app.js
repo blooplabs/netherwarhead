db251c73ca42f585d655e573e9439813ef087236
paperbd <heather.hb@gmail.com>
2015-04-05 22:36:07 -0700

    Using ngRoute and ngView instead of ngInclude for main navigation.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,25 @@
-var app = angular.module("netherWarhead", ["netherWarhead.controllers",
-  "netherWarhead.services", "netherWarhead.directives"]);
+var app = angular.module("netherWarhead", [
+  "ngRoute",
+  "netherWarhead.controllers",
+  "netherWarhead.services",
+  "netherWarhead.directives"
+]);
 var services = angular.module("netherWarhead.services", []);
 var controllers = angular.module("netherWarhead.controllers", []);
 var directives = angular.module("netherWarhead.directives", []);
+
+app.config(["$routeProvider",
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'views/chartsView.html',
+        controller: 'pageController'
+      }).
+      when('/about', {
+        templateUrl: 'views/aboutView.html',
+        controller: 'aboutController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
--- public/js/controllers/pageController.js
+++ public/js/controllers/pageController.js
@@ -1,27 +1,25 @@
 /*
- * Controller for navigating between pages
+ * Controller for navigating between partial templates
  * Requires app.js to be imported first
  */
 
-controllers.controller("pageController", ["$scope",
-  function pageController($scope) {
-
-    // Set up site-wide template
-    $scope.templates = {
-      "chartsView": "views/chartsView.html",
-      "aboutView": "views/aboutView.html"
-    };
-    $scope.template = $scope.templates.chartsView;
-    $scope.isActive = function (templateUrl) {
-        return templateUrl === $scope.template;
+controllers.controller("pageController", ["$scope", "$location",
+  function pageController($scope, $location) {
+    /*
+     * Set up site-wide templates
+     */
+    $scope.isActive = function(route) {
+        return route === $location.path();
     };
 
-    // Set up chart report templates
+    /*
+     * Set up chart report templates
+     */
     $scope.chartTemplates = {
       "all": "views/reports/allCharts.html",
       "subreddit": "views/reports/subredditCharts.html"
     };
-    $scope.chartTemplate =  $scope.templates.chartsView;
+    $scope.chartTemplate = "views/chartsView.html";
 
   }]
 );
